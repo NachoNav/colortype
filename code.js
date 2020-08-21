bf99e0c28135001d62b548964f26d659969dc6e1
NachoNav <jose.nfelix@alumnos.upm.es>
2020-08-21 17:01:09 +0200

    Added github icon
    It also changes color :)

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 var textArea = document.getElementsByName("textarea")[0];
+var icon = document.getElementsByClassName("github")[0];
 
 
 function color() {
     textArea.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
     textArea.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    icon.setAttribute("style", "filter: invert(1) sepia() saturate(10000%) hue-rotate(" + Math.floor(Math.random()*180) + "deg)");
 }
 
 textArea.addEventListener("keydown", color);
